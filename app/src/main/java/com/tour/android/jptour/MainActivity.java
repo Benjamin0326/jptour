68073c747d167ff0233ad0014c122effab4f370e
benjamin0326 <benjamin0326@naver.com>
2017-02-03 14:43:35 +0900

    splash

--- app/src/main/java/com/tour/android/jptour/MainActivity.java
+++ app/src/main/java/com/tour/android/jptour/MainActivity.java
@@ -1,13 +1,18 @@
 package com.tour.android.jptour;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Intent splashIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        splashIntent = new Intent(this, SplashActivity.class);
+        startActivity(splashIntent);
     }
 }
