5ef3d408ff36b8d41e65e2e242f5eb72f93116a8
benjamin0326 <benjamin0326@naver.com>
2017-02-03 14:33:56 +0900

    init

---
+++ app/src/main/java/com/tour/android/jptour/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tour.android.jptour;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
