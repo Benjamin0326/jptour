d3cf05ebcb8ce4a7297799464f6f23d6ec58a4a0
seokangsik <kaing1125@gmail.com>
2017-02-06 16:37:41 +0900

    commit

--- app/src/main/java/com/tour/android/jptour/SplashActivity.java
+++ app/src/main/java/com/tour/android/jptour/SplashActivity.java
@@ -1,8 +1,7 @@
 package com.tour.android.jptour;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
